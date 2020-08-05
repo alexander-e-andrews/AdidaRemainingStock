d7b97bcceff5d9ce430ab6576a75a26680b8c1ba
alexander-e-andrews <alexander.e.andrews.ce@gmail.com>
2020-08-05 00:53:24 -0400

    Fixed up files, and code that works better, but may not be more efficient

--- contentScript.js
+++ contentScript.js
@@ -21,11 +21,11 @@ function activateTimer(){
 } */
 
 chrome.runtime.onMessage.addListener(
-    async function(request, sender, sendResponse) {
-        console.log("recieved a message")
-        if (!await activateExtension()){
+    async function (request, sender, sendResponse) {
+        //console.log("recieved a message")
+        if (!await activateExtension()) {
             //CHANGE: find a way to actually check if teh page has been loaded
-            console.log("extension failed to run, try one more time.")
+            //console.log("extension failed to run, try one more time.")
             sleep(20)
             activateExtension()
         }
@@ -37,71 +37,81 @@ chrome.runtime.onMessage.addListener(
 //size-selector___RHMZk
 async function activateExtension() {
 
-    console.log("Activate Extension Called")
+    //console.log("Activate Extension Called")
     let elems = document.getElementsByClassName("product-description___1TLpA")
     //console.log(elems)
     //console.log(elems.length)
     if (elems.length >= 1) {
-        console.log(elems)
+        //console.log(elems)
         //console.log("We are in a product page")
         let id = extractProductID(location.href)
 
-        console.log(id)
+        //console.log(id)
         let data = await getAvailability(id)
         //console.log("Josh Lineman")
-        console.log(data)
+        //console.log(data)
         await addAvailability(data["variation_list"])
         return true
 
-    }else{
+    } else {
         console.log("Not Found")
         return false
 
     }
 }
 
-async function addAvailability(data){
+async function addAvailability(data) {
     //console.log(data)
     //#app > div > div > div > div > div.content-wrapper___3TFwT > div.sidebar-wrapper___3uF26 > div.sidebar___29cCJ > section > div.size-selector___RHMZk
     //document.querySelector("#app > div > div > div > div > div.content-wrapper___3TFwT > div.sidebar-wrapper___3uF26 > div.sidebar___29cCJ > section > div.size-selector___RHMZk")
     //document.querySelector("#app > div > div > div > div > div.content-wrapper___3TFwT > div > div.fixed-width-content___9S_-P > section.buy-section___ZPaYL.mobile___3NCgR > div.size-selector___RHMZk")
     let buttonContainer = document.querySelector("#app > div > div > div > div > div.content-wrapper___3TFwT > div.sidebar-wrapper___3uF26 > div.sidebar___29cCJ > section > div.size-selector___RHMZk")
     //CATCH For mobile
-    
-    if(!buttonContainer){
+
+    if (!buttonContainer) {
         buttonContainer = document.querySelector("#app > div > div > div > div > div.content-wrapper___3TFwT > div > div.fixed-width-content___9S_-P > section.buy-section___ZPaYL.mobile___3NCgR > div.size-selector___RHMZk")
+        console.log("Going mobile mode")
     }
-    //console.log(buttonContainer)
+    console.log("What the fuck")
+    console.log(buttonContainer)
+    //Should try using mutationobserver but its quite annoying
+    //await isItDone(buttonContainer)
+    console.log("AFTER mutation await")
     //Should fix this selector to something better
     let spanTargets = buttonContainer.querySelectorAll("button")
     //console.log(spanTargets)
-    if (spanTargets.length == 0){
-        await sleep(100)
-        spanTargets = buttonContainer.querySelectorAll("button")
+    while (true) {
+        if (spanTargets.length == 0) {
+            console.log("Span of buttons not found")
+            await sleep(100)
+            spanTargets = buttonContainer.querySelectorAll("button")
+        }else{
+            break
+        }
     }
     //console.log(spanTargets)
     spanTargets.forEach(element => {
         //console.log(element)
         let ins = element.querySelector("span")
-        for (x=0; x < data.length; x++){
+        for (x = 0; x < data.length; x++) {
             /* console.log(data[x].availability)
             console.log(data[x].size)
             console.log(ins.textContent) */
-            if(data[x].size == ins.textContent){
+            if (data[x].size == ins.textContent) {
                 ins.textContent = ins.textContent + ": " + data[x].availability
-                if(data[x].availability >= 15){
+                if (data[x].availability >= 15) {
                     ins.textContent = ins.textContent + "+"
                 }
                 break
             }
         }
-        
+
     });
 }
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
-  }
+}
 
 function extractProductID(url) {
     let id = url.split("/").pop() // Split on / and get final item
@@ -112,7 +122,7 @@ function extractProductID(url) {
 async function getAvailability(id) {
     let url = `https://www.adidas.com/api/products/tf/` + id + `/availability?sitePath=us`
 
-    const response = await fetch(url,{
+    const response = await fetch(url, {
         method: "GET"
     })
 
